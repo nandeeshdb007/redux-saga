98317071661ff3a0b17a5515705efd95063926b4
nandeesh_007 <nandeesh.db@bulkbeings.co>
2024-11-18 13:22:59 +0530

    cleared bug in addToCart Reducer

--- src/store/reducers/addToCartReducers.ts
+++ src/store/reducers/addToCartReducers.ts
@@ -1,20 +1,27 @@
-interface initialStateProps{
-    productId:string
+interface InitialStateProps {
+  productId: string;
 }
 
-const initialState:initialStateProps  = {
-  productId: "";
+const initialState: InitialStateProps = {
+  productId: "abckd",
+};
+
+interface ActionProps {
+  type: string;
+  payload?: string;
 }
 
-export const addToCartReducer = (initialState:initialStateProps, action:any) => {
-    switch(action.type){
-        case 'ADD_TO_CART_REDUCER':
-            return {
-                ...initialState,
-                productId: action.payload
-            }
-        default:
-             return initialState
-        
-    }
+export const addToCartReducer = (
+  state = initialState,
+  action: ActionProps
+): InitialStateProps => {
+  switch (action.type) {
+    case "ADD_TO_CART_REDUCER":
+      return {
+        ...state,
+        productId: action.payload || state.productId,
+      };
+    default:
+      return state;
+  }
 };
